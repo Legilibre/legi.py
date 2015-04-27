f5e97ce4f2f72c5a5791b50e10c069e59d70f366
Changaco <changaco@changaco.oy.lc>
2015-04-27 10:35:43 +0200

    add new script `anomalies.py`

---
+++ anomalies.py
@@ -0,0 +1,244 @@
+# encoding: utf8
+
+from __future__ import division, print_function, unicode_literals
+
+from argparse import ArgumentParser
+from datetime import date
+import re
+import sys
+
+from fr_calendar import (
+    MOIS_REPU, MOIS_REPU_MAP, republican_to_gregorian
+)
+from utils import connect_db, reconstruct_path, strip_down, strip_prefix
+
+
+MOIS_GREG = 'janvier février mars avril mai juin juillet août septembre octobre novembre décembre'.split()
+MOIS_GREG_MAP = {strip_down(m): i for i, m in enumerate(MOIS_GREG, 1)}
+NATURE_MAP = {
+    "ARRETE": "Arrêté",
+    "DECISION": "Décision",
+    "DECLARATION": "Déclaration",
+    "DECRET": "Décret",
+    "DECRET_LOI": "Décret-loi",
+    "LOI_CONSTIT": "Loi constitutionnelle",
+    "LOI_ORGANIQUE": "Loi organique",
+}
+NATURE_MAP_R = {v.upper(): k for k, v in NATURE_MAP.items()}
+
+
+jour_p = r'(?P<jour>1er|[0-9]{1,2})'
+mois_p = r'(?P<mois>%s)' % '|'.join(MOIS_GREG+MOIS_REPU)
+annee_p = r'(?P<annee>[0-9]{4,}|an [IVX]+)'
+numero_re = re.compile(r'n°(?!\s)', re.U)
+spaces_re = re.compile(r'\s+', re.U)
+word_re = re.compile(r'\w{2,}', re.U)
+
+ordure_p = r'quinquennale?'
+annexe_p = r"(?P<annexe>Annexe (au |à la |à l'|du ))"
+autorite_p = r'(?P<autorite>ministériel(le)?|du Roi|du Conseil d\'[EÉ]tat)'
+date_p = r'(du )?(%(jour_p)s )?%(mois_p)s( %(annee_p)s)?( (?P=annee))?' % globals()
+nature_p = r'(?P<nature>Arrêté|Code|Constitution|Convention|Décision|Déclaration|Décret(-loi)?|Loi( constitutionnelle| organique)?|Ordonnance)'
+numero_p = r'(n° ?)?(?P<numero>[0-9]+([\-–][0-9]+)*(, ?[0-9]+(-[0-9]+)*)*( et autres)?)\.?'
+titre1_re = re.compile(r'(%(annexe_p)s)?%(nature_p)s' % globals(), re.U | re.I)
+titre2_re = re.compile(r'( %(autorite_p)s| %(date_p)s| %(numero_p)s| %(ordure_p)s)' % globals(), re.U | re.I)
+
+
+def normalize_title(path, col, title):
+    if not title:
+        return title
+    title = spaces_re.sub(' ', title.strip())
+    if 'constitutionel' in title:
+        err(path, 'faute d\'orthographe dans le ', col, ': "constitutionel"')
+        title = title.replace('constitutionel', 'constitutionnel')
+    return title
+
+
+def parse_titre(path, col, titre):
+    m = titre1_re.match(titre)
+    if not m:
+        return {}, 0
+    d = m.groupdict()
+    duplicates = set()
+    while True:
+        pos = m.end()
+        m = titre2_re.match(titre, pos)
+        if not m:
+            return d, pos
+        for k, v in m.groupdict().items():
+            if v is None or k in duplicates:
+                continue
+            if k == 'numero':
+                v = v.replace('–', '-')
+            elif k == 'jour':
+                v = v.lower().replace('1er', '1')
+            if k not in d:
+                d[k] = v
+                continue
+            if d[k] == v or strip_down(d[k]) == strip_down(v):
+                continue
+            if k == 'numero':
+                a, b = sorted((d[k], v), key=len)
+                x, y = b.split('-', 1)
+                if a == x or a == y:
+                    d[k] = b
+                    continue
+            err(path, k, ': "', d[k], '" ≠ "', v, '" dans ', col)
+            duplicates.add(k)
+            d.pop(k)
+
+
+def upper_words_percentage(s):
+    words = word_re.findall(s)
+    return len([w for w in words if w.isupper()]) / len(words)
+
+
+def err(path, *a):
+    print(path, ': ', *a, sep='')
+    sys.stdout.flush()
+
+
+def anomalies_date_fin_etat(db):
+    a = [('articles', 'article'), ('textes_versions', 'texte/version')]
+    for table, sous_dossier in a:
+        q = db.all("""
+            SELECT dossier, cid, id, date_fin, etat
+              FROM {0}
+             WHERE date_fin <> '2999-01-01'
+               AND ( etat LIKE 'VIGUEUR%' AND date_fin < '{1}' OR
+                     etat NOT LIKE 'VIGUEUR%' AND etat <> 'ABROGE_DIFF' AND date_fin > '{1}'
+                   )
+        """.format(table, date.today().isoformat()))
+        for row in q:
+            dossier, cid, id, date_fin, etat = row
+            path = reconstruct_path(dossier, cid, sous_dossier, id)
+            x = 'passé' if etat.startswith('VIGUEUR') else 'futur'
+            err(path, 'la date de fin "', date_fin, '" est dans le ', x, ' mais l\'état est "', etat, '"')
+
+
+def anomalies_sections(db):
+    q = db.all("""
+        SELECT s.dossier, s.cid, section, num, debut, etat, count(*) as count
+          FROM sections_articles sa
+          JOIN sections s ON s.id = sa.section
+         WHERE etat NOT LIKE 'MODIF%'
+           AND lower(num) NOT LIKE 'annexe%'
+      GROUP BY section, num, debut, etat
+        HAVING count(*) > 1
+    """)
+    for row in q:
+        dossier, cid, section, num, debut, etat, count = row
+        path = reconstruct_path(dossier, cid, 'section_ta', section)
+        err(path, count, ' articles avec le numéro "', num,
+            '", la date de début "', debut,
+            '" et l\'état "', etat, '"')
+
+    q = db.all("""
+        SELECT DISTINCT s.dossier, s.cid, s.id, a.id, sa.etat, a.etat
+          FROM sections_articles sa
+          JOIN articles a ON a.id = sa.id AND a.etat <> sa.etat
+          JOIN sections s ON s.id = sa.section
+    """.format(date.today().isoformat()))
+    for row in q:
+        dossier, cid, id, a_id, sa_etat, a_etat = row
+        path = reconstruct_path(dossier, cid, 'section_ta', id)
+        err(path, 'l\'état "', sa_etat, '" ne correspond pas à l\'état "', a_etat,
+            '" dans le fichier de l\'article "', a_id, '"')
+
+
+def anomalies_textes_versions(db):
+    q = db.all("""
+        SELECT dossier, cid, id, titre, titrefull, nature, num, date_texte
+          FROM textes_versions
+    """)
+    for row in q:
+        dossier, cid, id, titre_o, titrefull_o, nature_o, num, date_texte = row
+        path = reconstruct_path(dossier, cid, 'texte/version', id)
+
+        num = num or ''
+        if num and num[-1] == '.':
+            err(path, 'num "', num, '" se termine par un point')
+            num = num[:-1]
+
+        titre, titrefull, nature = titre_o, titrefull_o, nature_o
+        len_titre = len(titre)
+        if len(titrefull) > len_titre:
+            if titrefull[len_titre] != ' ' and titrefull[:len_titre] == titre:
+                err(path, 'espace manquante dans titrefull après le ', len_titre, 'e caractère')
+                titrefull = titre + ' ' + titrefull[len_titre:]
+        titre = normalize_title(path, 'titre', titre)
+        titrefull = normalize_title(path, 'titrefull', titrefull)
+        if titre.endswith(' du'):
+            err(path, 'titre finit par "du"')
+            titre = titre[:-3]
+        len_titre = len(titre)
+        if len_titre > len(titrefull):
+            err(path, 'titre est plus long que titrefull')
+            titrefull = titre
+        if nature != 'CODE':
+            d1, endpos1 = parse_titre(path, 'titre', titre)
+            if not d1 and titre != 'Annexe' or d1 and endpos1 < len_titre:
+                err(path, 'titre est irrégulier: "', titre, '"')
+            d2, endpos2 = parse_titre(path, 'titrefull', titrefull)
+            if not d2:
+                err(path, 'titrefull est irrégulier: "', titrefull, '"')
+            if d1 or d2:
+                def get_key(key, ignore_not_found=False):
+                    g1, g2 = d1.get(key), d2.get(key)
+                    if not (g1 or g2) and not ignore_not_found:
+                        err(path, key, ' trouvé ni dans "', titre, '" (titre) ni dans "', titrefull, '" (titrefull)')
+                        return
+                    if g1 is None or g2 is None:
+                        return g1 if g2 is None else g2
+                    if strip_down(g1) == strip_down(g2):
+                        return g1
+                    if key == 'nature' and g1.split()[0] == g2.split()[0]:
+                        return g1 if len(g1) > len(g2) else g2
+                    err(path, key, ': "', g1, '" (dans titre) ≠ "', g2, '" (dans titrefull)')
+                annexe = get_key('annexe', ignore_not_found=True)
+                nature = get_key('nature').upper()
+                nature = NATURE_MAP_R.get(nature, nature)
+                if nature_o and nature != nature_o:
+                    err(path, 'nature: "', nature, '" (detectée) ≠ "', nature_o, '" (donnée)')
+                    if nature.split('_')[0] == nature_o.split('_')[0]:
+                        if len(nature_o) > len(nature):
+                            nature = nature_o
+                num_d = get_key('numero', ignore_not_found=True)
+                if annexe:  # On ne veut pas donner le numéro d'un décret à son annexe
+                    num_d = None
+                if num_d and '-' not in num_d:
+                    num_d = None
+                if num_d and num_d != num:
+                    err(path, 'numéro: "', num_d, '" (detecté) ≠ "', num, '" (donné)')
+                    if not num:
+                        num = num_d
+                if num and num == date_texte:
+                    err(path, 'num est égal à date_texte: "', num, '"')
+                    num = ''
+                jour = get_key('jour')
+                mois = get_key('mois')
+                annee = get_key('annee')
+                if jour and mois and annee:
+                    jour = int(jour.lower().replace('1er', '1'))
+                    if mois in MOIS_REPU_MAP:
+                        annee = strip_prefix(annee, 'an ')
+                        d = republican_to_gregorian(annee, mois, jour)
+                    else:
+                        mois = MOIS_GREG_MAP[strip_down(mois)]
+                        d = date(int(annee), mois, jour)
+                    date_texte_d = d.isoformat()
+                    if date_texte_d != date_texte:
+                        err(path, 'date: "', date_texte_d, '" (detectée) ≠ "', date_texte, '" (donnée)')
+
+
+def main(db):
+    anomalies_date_fin_etat(db)
+    anomalies_sections(db)
+    anomalies_textes_versions(db)
+
+
+if __name__ == '__main__':
+    p = ArgumentParser()
+    p.add_argument('db')
+    args = p.parse_args()
+    main(connect_db(args.db))
