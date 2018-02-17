7b55483b57c4c86ed4cdf46e1ff10192492dc291
Changaco <changaco@changaco.oy.lc>
2018-02-17 18:13:08 +0100

    download files in order

--- legi/download.py
+++ legi/download.py
@@ -22,11 +22,10 @@ def download_legi(dst_dir):
     ftph.connect(DILA_FTP_HOST, DILA_FTP_PORT)
     ftph.login()
     ftph.cwd(DILA_LEGI_DIR)
-    remote_files = {filename: {} for filename in ftph.nlst() if 'legi_' in filename}
-    local_set = set(local_files.keys())
-    remote_set = set(remote_files.keys())
-    common_files = list(local_set & remote_set)
-    missing_files = list(remote_set - local_set)
+    remote_files = [filename for filename in ftph.nlst() if 'legi_' in filename]
+    common_files = [f for f in remote_files if f in local_files]
+    missing_files = [f for f in remote_files if f not in local_files]
+    remote_files = {filename: {} for filename in remote_files}
     for filename in common_files:
         local_files[filename]['size'] = os.path.getsize(
             os.path.join(dst_dir, filename)
