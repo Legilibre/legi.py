c04e45e80b105947fb9cd717ca69be550baa5123
Julien Bouquillon <julien@revolunet.com>
2018-03-13 01:50:18 +0100

    better markdown

--- packages/dila.js/src/markdown.js
+++ packages/dila.js/src/markdown.js
@@ -1,10 +1,20 @@
 const unified = require("unified");
 const remarkStringify = require("remark-stringify");
-const cleanup = require("@z0mt3c/clean-html");
+const condense = require("condense-whitespace");
 const rehypeParse = require("rehype-parse");
 const rehype2remark = require("rehype-remark");
 
-const cleanBlocArticle = html => cleanup(html.replace(/&#13;/g, "").replace(/&amp;#13;/g, ""));
+const cleanBlocArticle = html =>
+  html
+    .replace(/\n/g, "<br/>")
+    //.replace(/\n\n\n+/g, "<br/><br/>")
+    //.replace(/\n\n+/g, "<br/>")
+    //.replace(/\n/g, "<br/>")
+    .replace(/&#x26;#13;/gm, "")
+    .replace(/&amp;#x26;#13;/gm, "")
+    .replace(/&amp;#13;/gm, "")
+    .replace(/&#13;/gm, "")
+    .replace(/&(amp;)?#13;/gm, "");
 
 const htmlToMdAst = async html => {
   const tree = unified()
@@ -35,7 +45,10 @@ const nodeMap = {
     type: "paragraph",
     children: [
       getHeading({ text: node.data.titre_ta, depth }),
-      /*getBreak(), getBreak(),*/ ...children
+      getBreak(),
+      getBreak(),
+      /* getBreak(),*/
+      ...children
     ]
   }),
   article: async (node, children, depth) => ({
@@ -43,10 +56,12 @@ const nodeMap = {
     children: [
       //...node,
       getHeading({ text: node.data.titre, depth }),
-      // getBreak(),
+      getBreak(),
+      getBreak(),
       // getBreak(),
       await htmlToMdAst(await cleanBlocArticle(node.data.bloc_textuel)),
-      ...children
+      ...children,
+      getBreak()
     ]
   }),
   default: (node, children) => ({
@@ -78,10 +93,7 @@ const toMarkdown = (node, options = DEFAULT_OPTIONS) => {
   if (options.tree) {
     return nodeToMdast(node);
   }
-  return nodeToMdast(node).then(x => {
-    console.log("x", x);
-    return stringifyMdast(x);
-  });
+  return nodeToMdast(node).then(stringifyMdast);
 };
 
 module.exports = toMarkdown;
