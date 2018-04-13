e4645b386e82b44b29252b7987d5c1098d149aff
Changaco <changaco@changaco.oy.lc>
2018-04-13 16:46:46 +0200

    create a module to facilitate spellchecking

---
+++ legi/spelling.py
@@ -0,0 +1,37 @@
+"""
+A spellchecker for French text, based on the `enchant` package.
+"""
+
+from __future__ import division, print_function, unicode_literals
+
+import re
+
+try:
+    from enchant.checker import SpellChecker
+    from enchant.tokenize import Filter
+except ImportError:
+    print("Warning: the `enchant` package is missing, spellchecking won't work")
+    SpellChecker = lambda *a, **kw: None
+    Filter = object
+
+from .roman import ROMAN_PATTERN_SIMPLE
+
+
+class RomanNumberFilter(Filter):
+
+    number_re = re.compile(r"^%s$" % ROMAN_PATTERN_SIMPLE)
+
+    def _skip(self, word):
+        return bool(self.number_re.match(word))
+
+
+french_checker = SpellChecker('fr_FR', filters=[RomanNumberFilter])
+
+
+if french_checker:
+    def spellcheck(text):
+        french_checker.set_text(text)
+        return not list(french_checker)
+else:
+    def spellcheck(text):
+        return
