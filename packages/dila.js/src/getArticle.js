6609ad6c3fb80ed046770c79c9de26f65f2def8e
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-01-03 03:25:53 +0100

    Article: better metas

--- packages/dila.js/src/getArticle.js
+++ packages/dila.js/src/getArticle.js
@@ -1,12 +1,23 @@
 const makeArticle = require("./makeArticle");
 const getArticleData = require("./getArticleData");
 
+const getLiens = (knex, { id }) =>
+  knex
+    .select("src_id", "dst_cid", "dst_id", "dst_titre", "typelien")
+    .from("liens")
+    .where({ src_id: id })
+    .orWhere({ dst_id: id });
+
 const getArticle = async (knex, { id }) => {
   const article = await getArticleData(knex, { id });
+  const liens = await getLiens(knex, { id });
   if (!article) {
     return null;
   }
-  return makeArticle(article);
+  return makeArticle({
+    ...article,
+    liens
+  });
 };
 
 module.exports = getArticle;
