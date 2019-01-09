f26aacf157483ca35556236a6dbe75474564c5f3
Changaco <changaco@changaco.oy.lc>
2019-01-09 18:22:53 +0100

    simplify and optimize the Record class

--- legi/db.py
+++ legi/db.py
@@ -76,43 +76,25 @@ def dict_factory(cursor, row):
     return {col[0]: row[idx] for idx, col in enumerate(cursor.description)}
 
 
-class Record:
-    __slots__ = ('_cols', '_vals')
-
-    def __init__(self, cols, values):
-        self._cols = {col: i for i, col in enumerate(cols)}
-        self._vals = values
-
-    def __getitem__(self, key):
-        return self._vals[key if type(key) is int else self._cols[key]]
+class Record(Row):
+    """This row type allows accessing columns as attributes.
+    """
 
     def __getattr__(self, col):
         try:
-            return self._vals[self._cols[col]]
+            return self[col]
         except KeyError:
             raise AttributeError(col)
 
-    def __len__(self):
-        return len(self._vals)
-
     def __repr__(self):
-        return ''.join('Record(', (
-            ', '.join(
-                '='.join(
-                    (col, repr(self._vals[i]))
-                    for col, i in self._cols
-                )
-            )
-        ), ')')
-
-
-def record_factory(cursor, row):
-    return Record((col[0] for col in cursor.description), row)
+        return 'Record(' + ', '.join(
+            '='.join((col, repr(self[col]))) for col in self.keys()
+        ) + ')'
 
 
 ROW_FACTORIES = {
     'dict': dict_factory,
-    'Record': record_factory,
+    'Record': Record,
     'Row': Row,
 }
 
