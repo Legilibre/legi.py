9996f842bf4befdbce59218a5bbb316df909ee19
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2019-04-15 17:04:46 +0200

    WIP calipsos

--- packages/api/src/routes/conteneurStructure.js
+++ packages/api/src/routes/conteneurStructure.js
@@ -14,16 +14,27 @@ const getStructure = tree =>
   }));
 
 const getSommaireData = memoize(
-  (baseDILA, id, date, includeArticles) => (
-    getDila(baseDILA).getSommaireConteneur({id, date, includeArticles})
-  ),
+  (baseDILA, id, date, includeArticles, includeCalipsos) =>
+    getDila(baseDILA).getSommaireConteneur({
+      id,
+      date,
+      includeArticles,
+      includeCalipsos
+    }),
   { promise: true }
 );
 
 routes.get("/conteneur/:conteneurId/structure", async (req, res) => {
   const date = new Date().toISOString().slice(0, 10);
   const includeArticles = req.query.includeArticles === 'true' || false;
-  const data = await getSommaireData(req.baseDILA, req.params.conteneurId, date, includeArticles);
+  const includeCalipsos = req.query.includeCalipsos === 'true' || false;
+  const data = await getSommaireData(
+    req.baseDILA,
+    req.params.conteneurId,
+    date,
+    includeArticles,
+    includeCalipsos
+  );
   res.json(getStructure(data));
 });
 
