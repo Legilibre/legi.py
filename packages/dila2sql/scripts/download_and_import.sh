bd3aa7b856cdd63c02c1cfd8ac5ee4c0b9077f58
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2019-04-19 11:59:17 +0200

    remove error

--- packages/dila2sql/scripts/download_and_import.sh
+++ packages/dila2sql/scripts/download_and_import.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-echo "Error blabla!" 1>&2
-exit 1
 python -m dila2sql.download --base KALI
 python -m dila2sql.importer --base KALI --raw postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/kali
