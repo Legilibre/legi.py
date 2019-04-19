7900cb388a2fc693607f8f242dd673db15dccb64
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2019-04-19 11:46:30 +0200

    force error

--- packages/dila2sql/scripts/download_and_import.sh
+++ packages/dila2sql/scripts/download_and_import.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
+dkjxzbvbkcbhxkz sdajkvjk vdfsv qeba
 
 python -m dila2sql.download --base KALI
-echo LOOOOL
 python -m dila2sql.importer --base KALI --raw postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/kali
-echo FINISHED
