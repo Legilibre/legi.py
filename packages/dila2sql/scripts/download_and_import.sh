f87b03179ca5473ea6363e48e4864c11230a6a24
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2019-04-19 10:51:34 +0200

    deploy dila2sql k8s

--- packages/dila2sql/scripts/download_and_import.sh
+++ packages/dila2sql/scripts/download_and_import.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 python -m dila2sql.download --base KALI
-python -m dila2sql.importer --base KALI --raw postgresql://legipy:dilamite@10.200.107.153/kali
+python -m dila2sql.importer --base KALI --raw postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/kali
