163c0530694c3356e45c1809207e80e9fed50347
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2019-04-19 11:50:07 +0200

    fix script download

--- packages/dila2sql/scripts/download_and_import.sh
+++ packages/dila2sql/scripts/download_and_import.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-dkjxzbvbkcbhxkz sdajkvjk vdfsv qeba
 
 python -m dila2sql.download --base KALI
 python -m dila2sql.importer --base KALI --raw postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/kali
