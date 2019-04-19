68921a3f1b42df19159a76ca84b3124231df6a2b
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2019-04-19 11:13:15 +0200

    testing

--- packages/dila2sql/scripts/download_and_import.sh
+++ packages/dila2sql/scripts/download_and_import.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 python -m dila2sql.download --base KALI
+echo LOOOOL
 python -m dila2sql.importer --base KALI --raw postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/kali
+echo FINISHED
