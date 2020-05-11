9c1ebbcdcb02bb81f6dbed35604ce13cecb45efd
Changaco <changaco@changaco.oy.lc>
2020-05-11 10:36:23 +0200

    simplify and optimize the FTP download code

--- legi/download.py
+++ legi/download.py
@@ -15,6 +15,8 @@ DILA_LEGI_DIR = '/LEGI'
 
 
 def download_legi(dst_dir, retry_hours=0):
+    if not os.path.exists(dst_dir):
+        os.mkdir(dst_dir)
     sleep_hours = 0
     while True:
         try:
@@ -31,9 +33,7 @@ def download_legi(dst_dir, retry_hours=0):
 
 
 def download_legi_via_ftp(dst_dir):
-    if not os.path.exists(dst_dir):
-        os.mkdir(dst_dir)
-    local_files = {filename: {} for filename in os.listdir(dst_dir)}
+    local_files = set(os.listdir(dst_dir))
     ftph = ftplib.FTP()
     ftph.connect(DILA_FTP_HOST, DILA_FTP_PORT)
     ftph.login()
@@ -41,41 +41,20 @@ def download_legi_via_ftp(dst_dir):
     remote_files = [filename for filename in ftph.nlst() if '.tar.' in filename]
     common_files = [f for f in remote_files if f in local_files]
     missing_files = [f for f in remote_files if f not in local_files]
-    remote_files = {filename: {} for filename in remote_files}
-    for filename in common_files:
-        local_files[filename]['size'] = os.path.getsize(
-            os.path.join(dst_dir, filename)
-        )
     ftph.voidcmd('TYPE I')
-    for filename in remote_files:
-        file_size = ftph.size(filename)
-        remote_files[filename]['size'] = int(file_size)
-    invalid_files = []
-    for filename in common_files:
-        if local_files[filename]['size'] < remote_files[filename]['size']:
-            invalid_files.append(filename)
-    print(
-        '{} remote files, {} common files ({} invalid), {} missing files'
-        .format(
-            len(remote_files),
-            len(common_files), len(invalid_files),
-            len(missing_files)
-        )
-    )
-    for filename in invalid_files:
-        filepath = os.path.join(dst_dir, filename)
-        with open(filepath, mode='a+b') as fh:
-            print('Continuing the download of the file {}'.format(filename))
-            ftph.retrbinary(
-                'RETR {}'.format(filename),
-                fh.write,
-                rest=local_files[filename]['size']
-            )
+    print('{} remote files, {} common files, {} missing files'.format(
+        len(remote_files), len(common_files), len(missing_files),
+    ))
     for filename in missing_files:
         filepath = os.path.join(dst_dir, filename)
-        with open(filepath, mode='wb') as fh:
-            print('Downloading the file {}'.format(filename))
-            ftph.retrbinary('RETR {}'.format(filename), fh.write, rest=0)
+        with open(filepath + '.part', mode='a+b') as fh:
+            offset = fh.tell()
+            if offset:
+                print('Continuing the download of the file ' + filename)
+            else:
+                print('Downloading the file ' + filename)
+            ftph.retrbinary('RETR ' + filename, fh.write, rest=offset)
+        os.rename(filepath + '.part', filepath)
     ftph.quit()
 
 
