74f758690452b0979e65cc2e87ba292f060f0b6d
Léo El Amri <leo@superlel.me>
2022-04-30 14:51:23 +0200

    Using automatic script creation
    New commands are:
    * legi-download
    * legi-tar2sqlite
    * legi-normalize
    * legi-factorize
    * legi-html
    * legi-anomalies

--- tests/test_factorize.py
+++ tests/test_factorize.py
@@ -1,7 +1,7 @@
 from legi.db import connect_db
-from legi.factorize import main
+from legi.factorize import run
 
 
 def test_factorize():
     db = connect_db(':memory:')
-    main(db)
+    run(db)
