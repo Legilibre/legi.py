48be010c3214ed71dbb1223e10b8cc08ad3ba7c9
Changaco <changaco@changaco.oy.lc>
2026-08-09 11:53:46 +0000

    process the liste_suppression_legi_dossier.dat file

--- legi/tar2sqlite.py
+++ legi/tar2sqlite.py
@@ -30,6 +30,18 @@ SOUS_DOSSIER_MAP = {
     'textes_structs': 'texte/struct',
     'textes_versions': 'texte/version',
 }
+TABLES_MAP = {'ARTI': 'articles', 'SCTA': 'sections', 'TEXT': 'textes_{}s'}
+
+suppress_re = re.compile(
+    r"legi/global/code_et_TNC_(en|non)_vigueur/"
+    r"(?P<dossier>(?:code|TNC)_\1_vigueur)/?"
+    r"(?:[A-Z]{4}/){2}(?:[0-9]{2}/){5}(?P<cid>[A-Z0-9]{20})/?"
+    r"(?:"
+        r"(?P<sous_dossier>article|section_ta|texte/(?:struct|version))/"
+        r"(?:(?:[A-Z]{4}/){2}(?:[0-9]{2}/){5})?"
+        r"(?P<id>[A-Z0-9]{20})"
+    r")?"
+)
 
 
 def innerHTML(e):
@@ -37,26 +49,20 @@ def innerHTML(e):
     return r[r.find('>')+1:-len(e.tag)-3]
 
 
-def suppress(get_table, db, liste_suppression):
+def suppress(db, liste_suppression, nom_liste):
     counts = defaultdict(int)
-    for path in liste_suppression:
-        parts = path.split('/')
-        assert parts[0] == 'legi'
-        row_cid = parts[11]
-        row_id = parts[-1]
-        assert len(row_id) == 20
-        table = get_table(parts)
-        sous_dossier = SOUS_DOSSIER_MAP[table]
+
+    def delete(table, row_cid, row_id, dossier, sous_dossier):
         db.run("""
             DELETE FROM {0}
              WHERE dossier = ?
                AND cid = ?
                AND id = ?
-        """.format(table), (parts[3], row_cid, row_id))
+        """.format(table), (dossier, row_cid, row_id))
         changes = db.changes()
         if changes:
             counts['delete from ' + table] += changes
-            # Also delete derivative data
+            # Also delete secondary data
             if table in ('articles', 'textes_versions'):
                 db.run("""
                     DELETE FROM liens
@@ -120,10 +126,32 @@ def suppress(get_table, db, liste_suppression):
                    AND cid = ?
                    AND sous_dossier = ?
                    AND id = ?
-            """, (parts[3], row_cid, sous_dossier, row_id))
+            """, (dossier, row_cid, sous_dossier, row_id))
             counts['delete from duplicate_files'] += db.changes()
+
+    for path in liste_suppression:
+        if not path:
+            continue
+        m = suppress_re.fullmatch(path)
+        assert m, path
+        if m['id']:
+            table = TABLES_MAP[m['id'][4:8]]
+            if table == 'textes_{}s':
+                table = table.format(m['sous_dossier'].split('/')[-1])
+                assert table in SOUS_DOSSIER_MAP
+            delete(table, m['cid'], m['id'], m['dossier'], m['sous_dossier'])
+        else:
+            for table, sous_dossier in SOUS_DOSSIER_MAP.items():
+                row_ids = db.all("""
+                    SELECT id
+                      FROM {0}
+                     WHERE dossier = ?
+                       AND cid = ?
+                """.format(table), (m['dossier'], m['cid']))
+                for row_id in row_ids:
+                    delete(table, m['cid'], row_id, m['dossier'], sous_dossier)
     total = sum(counts.values())
-    print("made", total, "changes in the database based on liste_suppression_legi.dat:",
+    print(f"made {total} changes in the database based on {nom_liste}:",
           json.dumps(counts, indent=4, sort_keys=True))
 
 
@@ -144,7 +172,6 @@ def process_archive(
     META_VERSION_TAGS = set(
         'TITRE TITREFULL ETAT DATE_DEBUT DATE_FIN AUTORITE MINISTERE'.split()
     )
-    TABLES_MAP = {'ARTI': 'articles', 'SCTA': 'sections', 'TEXT': 'textes_'}
     TYPELIEN_MAP = {
         "ABROGATION": "ABROGE",
         "ANNULATION": "ANNULE",
@@ -171,8 +198,9 @@ def process_archive(
 
     def get_table(parts):
         table = TABLES_MAP[parts[-1][4:8]]
-        if table == 'textes_':
-            table += parts[13] + 's'
+        if table == 'textes_{}s':
+            table = table.format(parts[13])
+            assert table in SOUS_DOSSIER_MAP
         return table
 
     soft_hyphens = defaultdict(list)
@@ -197,8 +225,9 @@ def process_archive(
 
     counts = defaultdict(int)
     skipped = 0
-    unknown_folders = {}
+    unknown_folders = defaultdict(int)
     liste_suppression = []
+    liste_suppression_dossier = []
     xml = etree.XMLParser(remove_blank_text=True)
     with tqdm(total=os.stat(archive_path).st_size, unit='bytes') as pbar, \
          open(archive_path, 'rb') as file, \
@@ -211,17 +240,21 @@ def process_archive(
                 continue
             parts = path.split('/')
             if parts[-1] == 'liste_suppression_legi.dat':
-                liste_suppression += b''.join(entry.get_blocks()).decode('ascii').split()
+                liste_suppression.extend(
+                    b''.join(entry.get_blocks()).decode('ascii').split()
+                )
+                continue
+            if parts[-1] == 'liste_suppression_legi_dossier.dat':
+                liste_suppression_dossier.extend(
+                    b''.join(entry.get_blocks()).decode('ascii').split(' D\n')
+                )
                 continue
             if parts[1] == 'legi':
                 path = path[len(parts[0])+1:]
                 parts = parts[1:]
             if not parts[2].startswith('code_et_TNC_'):
                 # https://github.com/Legilibre/legi.py/issues/23
-                try:
-                    unknown_folders[parts[2]] += 1
-                except KeyError:
-                    unknown_folders[parts[2]] = 1
+                unknown_folders['/'.join(parts[:3])] += 1
                 continue
             dossier = parts[3]
             row_cid = parts[11]
@@ -486,7 +519,9 @@ def process_archive(
             print("skipped", x, "files in unknown folder `%s`" % d)
 
     if liste_suppression:
-        suppress(get_table, db, liste_suppression)
+        suppress(db, liste_suppression, 'liste_suppression_legi.dat')
+    if liste_suppression_dossier:
+        suppress(db, liste_suppression_dossier, 'liste_suppression_legi_dossier.dat')
 
     if not raw:
         remove_detected_soft_hyphens(db, soft_hyphens)
